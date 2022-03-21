097a527a765b51d2d27c63829b672c056fc53460
Abdi <letsbuildshhopify>
2022-03-21 19:12:19 +0000

    Responsive Layout + content

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
-const image1 = document.getElementById ('image1');
-const image2 = document.getElementById ('image2');
-const image3 = document.getElementById ('image3');
 const textBox =  document.getElementById ('text-box');
 const nav = document.getElementById('nav');
 const toggleIcon = document.getElementById ('toggle-icon');
@@ -9,26 +6,20 @@ const toggleIcon = document.getElementById ('toggle-icon');
 // Drk Mode Styles
 function darkMode(){
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
-    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = "Dark Mode";
     toggleIcon.children[1].classList.remove('fa-sun');
     toggleIcon.children[1].classList.add('fa-moon');
-    image1.src = 'img/undraw_businesswoman_re_5n6b_dark.svg'
-    image2.src = 'img/undraw_business_shop_qw-5-t_dark.svg'
-    image3.src = 'img/undraw_business_plan_re_0v81_dark.svg'
+
 }
 
 
 // Light Mode Styles
 function lightMode(){
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
-    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = "Light Mode";
     toggleIcon.children[1].classList.remove('fa-moon');
     toggleIcon.children[1].classList.add('fa-sun');
-    image1.src = 'img/undraw_businesswoman_re_5n6b_light.svg'
-    image2.src = 'img/undraw_business_shop_qw-5-t_light.svg'
-    image3.src = 'img/undraw_business_plan_re_0v81_light.svg'
+
 }
 
 
