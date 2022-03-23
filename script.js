495fff1a4e663a800b6e39b0a898252db29ca689
Abdi <letsbuildshhopify>
2022-03-23 17:11:04 +0000

    Scroll Animations + copy of css sheet

--- script.js
+++ script.js
@@ -71,9 +71,6 @@ function switchTheme(event){
     
 }
 
-// event listeners
-toggleSwitch.addEventListener('change', switchTheme);
-menuBars.addEventListener('click', toggleNav);
 
 // checking local storage for theme
 const currentTheme = localStorage.getItem('theme');
@@ -90,4 +87,104 @@ if(currentTheme){
         lightMode()
     };
 } 
+
+// Js on scroll Animation
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal")
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTOP = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 0;
+
+        if (revealTOP < windowHeight - revealPoint) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+function reveal2() {
+    var reveals2 = document.querySelectorAll(".reveal2")
+    for (var i = 0; i < reveals2.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTOP = reveals2[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTOP < windowHeight - revealPoint) {
+            reveals2[i].classList.add("active2");
+
+        } else {
+            reveals2[i].classList.remove("active2");
+        }
+    }
+};
+
+
+function reveal3() {
+    var reveals3 = document.querySelectorAll(".reveal3")
+    for (var i = 0; i < reveals3.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTOP = reveals3[i].getBoundingClientRect().top;
+        var revealPoint = 0;
+
+        if (revealTOP < windowHeight - revealPoint) {
+            reveals3[i].classList.add("active3");
+
+        } else {
+            reveals3[i].classList.remove("active3");
+        }
+    }
+};
+
+function reveal4() {
+    var reveals4 = document.querySelectorAll(".reveal4")
+    for (var i = 0; i < reveals4.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTOP = reveals4[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTOP < windowHeight - revealPoint) {
+            reveals4[i].classList.add("active4");
+
+        } else {
+            reveals4[i].classList.remove("active4");
+        }
+    }
+};
+
+function reveal5() {
+    var reveals5 = document.querySelectorAll(".reveal5")
+    for (var i = 0; i < reveals5.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTOP = reveals5[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+
+        if (revealTOP < windowHeight - revealPoint) {
+            reveals5[i].classList.add("active5");
+
+        } else {
+            reveals5[i].classList.remove("active5");
+        }
+    }
+};
+
+
+
+
+
+
+
+
+// event listeners
+toggleSwitch.addEventListener('change', switchTheme);
+menuBars.addEventListener('click', toggleNav);
+window.addEventListener("scroll", reveal);
+window.addEventListener("scroll", reveal2);
+window.addEventListener("scroll", reveal3);
+window.addEventListener("scroll", reveal4);
+window.addEventListener("scroll", reveal5);
+
+
 createEventListener()
